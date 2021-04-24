b154a0dd319eba4af8194e7998ac0aa22a75a9c5
Filip <31472895+Filiq@users.noreply.github.com>
2021-04-24 22:59:20 +0200

    Add files via upload

---
+++ compareIntegers/compareIntegers.ts
@@ -0,0 +1,11 @@
+function compareIntegers(a: string, b: string): string {
+    if(parseInt(a) < parseInt(b)) return "less"
+    
+    if(parseInt(a) > parseInt(b)) return "greater"
+    
+    return "equal"
+}
+
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
