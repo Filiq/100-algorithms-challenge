b46a4d7cc95189adcc0d56bdf893b3e0fd70de61
Filip <31472895+Filiq@users.noreply.github.com>
2021-04-28 20:51:36 +0200

    Add files via upload

---
+++ houseOfCats/houseOfCats.ts
@@ -0,0 +1,16 @@
+function houseOfCats(legs: number): number[] {
+    const people: number[] = [];
+
+    if (legs === 2) return [1]
+    
+    while(legs >= 0){
+        people.unshift(legs/2);
+        legs -= 4;
+    }
+
+
+    return people;
+}
+
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
