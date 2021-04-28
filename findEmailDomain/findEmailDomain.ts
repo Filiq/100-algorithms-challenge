268cb1e3feccf023d4852bc9838ea13c7f0d268a
Filip <31472895+Filiq@users.noreply.github.com>
2021-04-28 19:31:50 +0200

    Add files via upload

---
+++ findEmailDomain/findEmailDomain.ts
@@ -0,0 +1,8 @@
+function findEmailDomain(address: string): string {
+    const lastIndex = address.lastIndexOf("@");
+
+    return address.slice(lastIndex + 1);
+}
+
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
