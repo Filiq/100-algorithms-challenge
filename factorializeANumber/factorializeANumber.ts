5ce9d001b617563fd0c7764025c0933cfe55c47c
Filip <31472895+Filiq@users.noreply.github.com>
2021-04-28 19:25:03 +0200

    Add files via upload

---
+++ factorializeANumber/factorializeANumber.ts
@@ -0,0 +1,12 @@
+function factorializeANumber(num: number): number {
+    let factorial = 1;
+
+    for(let i = 2; i <= num; i++) {
+        factorial = factorial * i;
+    }
+
+    return factorial;
+}
+
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
