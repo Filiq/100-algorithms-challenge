af165c47fd18b8a9c783784da4ac0f93988b0ef3
Filip <31472895+Filiq@users.noreply.github.com>
2021-04-29 20:48:40 +0200

    Add files via upload

---
+++ largestOfFour/largestOfFour.ts
@@ -0,0 +1,12 @@
+function largestOfFour(nums: number[][]): number[] {
+    let largestNrs = [];
+
+    nums.forEach(num => {
+        largestNrs.push(num.sort((a,b) => a-b)[3]);
+    })
+
+    return largestNrs;
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
