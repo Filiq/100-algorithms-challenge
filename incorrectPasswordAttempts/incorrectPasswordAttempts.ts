729c7a286b8d565c89fd4ee01fd72b16d5d8357e
Filip <31472895+Filiq@users.noreply.github.com>
2021-05-02 22:25:56 +0200

    Add files via upload

---
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -0,0 +1,13 @@
+function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+    let failCount = 0;
+
+    for(let i = 0; i < attempts.length; i++){
+        failCount = attempts[i] === passcode ? 0 : failCount++;
+
+        if(failCount === 10) return true;
+    }
+
+    return false;
+}
+
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
